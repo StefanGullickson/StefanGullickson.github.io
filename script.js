84a421297f312e74c92b95fe4af8e1f0c6d63f16
quentinrh12 <quentinrh12@gmail.com>
2021-10-06 13:37:09 -0500

    java script file

---
+++ script.js
@@ -0,0 +1,9 @@
+var up = document.getElementById('MCG_UP'); 
+    up.innerHTML = "Click button to start"; 
+    var down = document.getElementById('MCG_DOWN'); 
+    function MCG_Pic() {
+        var img = document.createElement('img');
+        img.src = 'mac-guessr/mcgpic.png';
+        document.getElementById('body').appendChild(img);
+        down.innerHTML = " ";
+    } 
